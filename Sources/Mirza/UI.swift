d66c8dedb089120abedd3959ffcb80216f84c3af
dleuck <dan@ikayzo.com>
2023-03-18 13:42:00 +0900

    Fixed precision issue in UI.rgb

--- Sources/Mirza/UI.swift
+++ Sources/Mirza/UI.swift
@@ -8,9 +8,9 @@
 import SwiftUI
 
 public func rgb(_ r: Double, _ g: Double, _ b: Double, alpha: Double = 100) -> Color {
-    return Color(red: r / 100, green: g / 100, blue: b / 100, opacity: alpha / 100)
+    return Color(red: r / 100.0, green: g / 100.0, blue: b / 100.0, opacity: alpha / 100.0)
 }
 
 public func gray(_ gray: Double, alpha: Double = 100) -> Color {
-    return Color(red: gray / 100, green: gray / 100, blue: gray / 100, opacity: alpha / 100)
+    return Color(red: gray / 100.0, green: gray / 100.0, blue: gray / 100.0, opacity: alpha / 100.0)
 }
