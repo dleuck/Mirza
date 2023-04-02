0cb1da0451468cbcf2ffd2b33c1c065062625952
dleuck <dan@ikayzo.com>
2023-04-02 18:09:36 +0900

    Fixed bug in color mix test

--- Tests/MirzaTests/RGBTests.swift
+++ Tests/MirzaTests/RGBTests.swift
@@ -14,7 +14,7 @@ final class RGBTests: XCTestCase {
 
     func testMix() throws {
         XCTAssertEqual(RGB.red.mix(RGB.white, percent: 0.2), RGB(100, 20, 20))
-        XCTAssertEqual(RGB.red.mix(RGB.red), RGB.blue)
+        XCTAssertEqual(RGB.red.mix(RGB.red), RGB.red)
         XCTAssertEqual(RGB.blue.mix(RGB.blue, percent: 0.5), RGB.blue)
     }
     
