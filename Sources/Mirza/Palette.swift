89d4045c4dca7f9722be9e8a3b7f2c735e3c11a4
dleuck <dan@ikayzo.com>
2023-04-05 07:42:55 +0900

    Updated Social Palette

--- Sources/Mirza/Palette.swift
+++ Sources/Mirza/Palette.swift
@@ -45,7 +45,8 @@ public struct Palette: Codable, CustomStringConvertible, Identifiable, Hashable,
     )
     
     public static let social = Palette(
-        name: "social", webRGB(12, 198, 171), webRGB(84, 33, 244), webRGB(206, 158, 5)
+        name: "social", webRGB(12, 198, 171), webRGB(84, 33, 244), webRGB(203, 148, 1),
+            webRGB(214, 101, 1)
     )
     
     public static let nouveau = Palette(
